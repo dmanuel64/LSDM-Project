bc5ada15d1e344ac36d2825855cac3b383934662
qui834 <61246356+qui834@users.noreply.github.com>
2021-04-20 21:32:36 -0500

    Outline working

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from mysite import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls)
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
