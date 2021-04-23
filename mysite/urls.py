208016489c7e46516273d12a8c95b15a7bd4c6d0
qui834 <61246356+qui834@users.noreply.github.com>
2021-04-23 17:14:06 -0500

    Charts working

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls)
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
