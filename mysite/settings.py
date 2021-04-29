02b17d19a72a7508b993e6de8d3efdb2941d9579
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-04-29 13:26:18 -0500

    Changed logout redirection url in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,5 +134,6 @@ DEGUG = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 LOGIN_REDIRECT_URL =  '/'
+LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
