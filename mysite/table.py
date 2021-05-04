6b3e4e42a764278d8b980a4d1f0ea4214158350e
ldselvera <luiselvera97@gmail.com>
2021-05-03 21:07:00 -0500

    documentation for visualization functions, added table and search functions

---
+++ mysite/table.py
@@ -0,0 +1,65 @@
+import os
+import re
+import pandas as pd
+from textblob import TextBlob
+
+
+class Table:
+
+    def search(self, query):
+        df = pd.read_csv("data/vaccination_tweets.csv")
+
+        df.drop(columns=['id'], inplace=True)
+        df.dropna()
+        type = query.split(":")[0]
+
+        keywords = query.split(":")[1]
+        keywords = keywords.strip()
+
+        results = df[df[type].str.contains("(?i)"+keywords) == True]
+
+        return results
+
+    def input_sentimental(self, sentence):
+        polarity = TextBlob(sentence).sentiment.polarity
+        subjectivity = TextBlob(sentence).sentiment.subjectivity
+
+        return ( polarity, subjectivity)
+
+    def top_retweet_favorites(self, k, engagement_type):
+        df = pd.read_csv("data/vaccination_tweets.csv")
+
+        if engagement_type == "retweets":
+            df = df.sort_values(by='retweets', ascending=False)[['text', 'date', 'user_name', 'user_location', 'hashtags', 'favorites', 'retweets']].head(n=k)
+        elif engagement_type == "favorites":            
+            df = df.sort_values(by='favorites', ascending=False)[['text', 'date', 'user_name', 'user_location', 'hashtags', 'favorites', 'retweets']].head(n=k)
+
+        return df
+
+    def top_polarity(self, sentiment_type, k):
+        df = pd.read_csv("data/vaccination_tweets.csv")
+        df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['subjectivity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+
+        if sentiment_type == "negative":
+            # inspect the most negatively charged tweets
+            df = df.sort_values(by='polarity', ascending=True)[['text', 'polarity', 'subjectivity']].reset_index(drop=True).head(n=k)
+        elif sentiment_type == "positive":
+            # inspect the most positively charged tweets
+            df = df.sort_values(by='polarity', ascending=False)[['text', 'polarity', 'subjectivity']].reset_index(drop=True).head(n=k)
+
+        return df
+
+    def top_subjectivity(self, subjectivity_type, k):    
+        df = pd.read_csv("data/vaccination_tweets.csv")
+        df['polarity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['subjectivity'] = df['text'].apply(lambda x: TextBlob(x).sentiment.subjectivity)
+
+        if subjectivity_type == "objective":
+            # inspect the most objective tweets            
+            df = df.sort_values(by='subjectivity', ascending=True)[['text', 'polarity', 'subjectivity']].reset_index(drop=True).head(n=k)
+        if subjectivity_type == "subjective":
+            # inspect the most subjective tweets (NOTE: subjectivity scale ranges from 0 to 1)
+            df = df.sort_values(by='subjectivity', ascending=False)[['text', 'polarity', 'subjectivity']].reset_index(drop=True).head(n=k)
+
+        return df
--- mysite/visualization.py
+++ mysite/visualization.py
@@ -18,6 +18,13 @@ class Bar:
         self.all_vax = ['covaxin', 'sinopharm', 'sinovac', 'moderna', 'pfizer', 'biontech', 'oxford', 'astrazeneca', 'sputnik']
 
     def monthly_tweets(self):
+        """[Get monthly number of tweets]
+
+        Returns:
+            [string]: [month labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         df['date']=pd.to_datetime(df.date)
         df = df.sort_values(by='date')
@@ -27,9 +34,16 @@ class Bar:
         line['count'] = line['text']
         line = line[['date', 'count']]
 
-        return ( list(line['date'].values), list(line['count'].values))
+        return list(line['date'].values), list(line['count'].values)
 
     def tweets_bar(self):
+        """[Get daily number of tweets]
+
+        Returns:
+            [string]: [day labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         df['date'] =pd.to_datetime(df.date)
         df = df.sort_values(by='date')
@@ -41,9 +55,19 @@ class Bar:
         timeline['count'] = timeline['text']
         timeline = timeline[['date', 'count']]
 
-        return (list(timeline['date'].values), list(timeline['count'].values))
+        return list(timeline['date'].values), list(timeline['count'].values)
 
     def user_engagement_bar(self, engagement_type):
+        """[Get users with most retweets, favorites, and engagements]
+
+        Args:
+            engagement_type ([string]): ["retweets", "favorites", "total_engagement]
+
+        Returns:
+            [string]: [user name labels]
+            [integers]: [values]
+        """        
+
         df=pd.read_csv('data/vaccination_tweets.csv')
         df['total_engagement']=df['retweets']+df['favorites']
 
@@ -59,9 +83,16 @@ class Bar:
             data = df.groupby('user_name',as_index=False).agg({'total_engagement':'sum'}).sort_values('total_engagement',ascending=False).head(10)
             y = "total_engagement"            
 
-        return (list(data['user_name'].values), list(data[y].values))
+        return list(data['user_name'].values), list(data[y].values)
 
     def verified_bar(self):
+        """[Get count of verified and non verified accounts]
+
+        Returns:
+            [string]: [verified or non verified labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         df.drop(columns=['id'], inplace=True)
         df = df.drop_duplicates('text')
@@ -77,13 +108,29 @@ class Bar:
         return (labels, data)
 
     def source_max(self, k):
+        """[Get top sources]
+
+        Args:
+            k ([integer]): [top k sources]
+
+        Returns:
+            [string]: [sources labels]
+            [integers]: [values]
+        """        
         df = pd.read_csv("data/vaccination_tweets.csv")
         src = df['source'].value_counts().sort_values(ascending=False)
-        source = src.head(5)
+        source = src.head(k)
 
-        return (list(source.index), list(source.values))
+        return list(source.index), list(source.values)
 
     def per_day(self):
+        """[Get number of tweets per weekdays]
+
+        Returns:
+            [string]: [weekday labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         df["date"] = pd.to_datetime(df["date"])
         df["Month"] = df["date"].apply(lambda x : x.month)
@@ -99,19 +146,39 @@ class Bar:
         df.day = pd.Categorical(df.day,categories=cats)
         df = df.sort_values('day')
         
-        return (list(df['day'].values), list(df['count'].values))
+        return list(df['day'].values), list(df['count'].values)
 
     def top_hashtag(self, k):
+        """[Get top hashtags]
+
+        Args:
+            k ([integer]): [top k hashtags]
+
+        Returns:
+            [string]: [hashtags labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         htags = pd.DataFrame(df['hashtags'].value_counts()).reset_index()
         htags = htags[htags['index'] != 'No HashTag']
         htags = htags.rename(columns={'index': 'Hashtags', 'hashtags': 'counts'})[:k]
 
-        return(list(htags['Hashtags'].values), list(htags['counts'].values))
+        return list(htags['Hashtags'].values), list(htags['counts'].values)
 
     def hashtag_retweet_favorite(self, engagement_type, k):
-        df = pd.read_csv("data/vaccination_tweets.csv")
+        """[Get hashtags with most retweets or favorites]
 
+        Args:
+            engagement_type ([string]): ["retweets" or "favorites]
+            k ([integer]): [top k hashtags with most specific engagement type]
+
+        Returns:
+            [string]: [hashtags labels]
+            [integers]: [retweets or favorites values]
+        """        
+
+        df = pd.read_csv("data/vaccination_tweets.csv")
         df = df[pd.notnull(df['hashtags'])]
 
         if engagement_type == "retweets":
@@ -119,14 +186,36 @@ class Bar:
         elif engagement_type == "favorites":
             df_sort = df.sort_values('favorites', ascending=False)
 
-        return (list(df_sort[:k]['hashtags'].values), list(df_sort[:k][engagement_type].values))  
+        return list(df_sort[:k]['hashtags'].values), list(df_sort[:k][engagement_type].values)  
 
+    # 
     def top_location(self, k):
+        """[Get top locations]
+
+        Args:
+            k ([integer]): [top k locations]
+
+        Returns:
+            [string]: [location labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         data = df["user_location"].value_counts().values[0:5], df["user_location"].value_counts().index[:k]
-        return (list(data[1]), list(data[0]))
+
+        return list(data[1]), list(data[0])
 
     def daily_engagement(self, engagement_type):
+        """[Get daily engagement such as retweets, favorites, or engagement]
+
+        Args:
+            engagement_type ([string]): ["retweet", "favorites", "total engagement]
+
+        Returns:
+            [string]: [date labels]
+            [integers]: [values]
+        """
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         df['date']=pd.to_datetime(df['date'])
 
@@ -144,9 +233,16 @@ class Bar:
             df['date'] = df['date'].dt.strftime('%d/%m/%y')       
             day_month = df.groupby('date', sort=False, as_index=False).agg({'favorites':'sum'})            
 
-        return (list(day_month['date'].values), list(day_month[engagement_type].values))   
+        return list(day_month['date'].values), list(day_month[engagement_type].values)   
 
     def sentiment_bar(self):
+        """[Get total sentimental analysis]
+
+        Returns:
+            [string]: [sentiment labels]
+            [integers]: [values]
+        """
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         analyser = SentimentIntensityAnalyzer()
 
@@ -167,10 +263,20 @@ class Bar:
         df['sentiment']=pd.Series(np.array(sentiment))
         temp = df.groupby('sentiment').count()['text'].reset_index().sort_values(by='text',ascending=False)
 
-        return (list(temp['sentiment'].values), list(temp['text'].values))
-
+        return list(temp['sentiment'].values), list(temp['text'].values)
 
     def accounts_sentiment(self, sentiment_type, k):
+        """[Get top accounts with most positive, neutral, or negative]
+
+        Args:
+            sentiment_type ([string]): ["positive", "neutral", "negative"]
+            k ([integer]): [description]
+
+        Returns:
+            [string]: [user_name labels]
+            [integers]: [values]
+        """        
+
         df = pd.read_csv("data/vaccination_tweets.csv")
         analyser = SentimentIntensityAnalyzer()
 
@@ -200,11 +306,16 @@ class Bar:
             data=df[(df.sentiment== "Negative")].drop_duplicates(subset=["user_name"])\
                 .sort_values(by=["user_followers"], ascending=False)[["user_name", "user_followers"]][:k]                                
 
-        return (list(data['user_name'].values), list(data['user_followers'].values))      
+        return list(data['user_name'].values), list(data['user_followers'].values)     
 
 class Pie:
-
     def verified_pie(self):
+        """[Get number of verified vs unverified]
+
+        Returns:
+            [string]: [verified or non verified labels]
+            [integers]: [values]
+        """        
         df = pd.read_csv("data/vaccination_tweets.csv")
         verified = df[df["user_verified"]==True].shape[0]
         non_verified = df[df["user_verified"]==False].shape[0]
@@ -212,15 +323,31 @@ class Pie:
         labels = ['not_verified', 'verified']
         sizes = [non_verified, verified]
 
-        return (labels, sizes)
+        return labels, sizes
 
     def top_pie(self, k, col_name):
+        """[Get top information ]
+
+        Args:
+            k ([integer]): [top k information]
+            col_name ([string]): [information of interest]
+
+        Returns:
+            [string]: [top information labels]
+            [integers]: [values]
+        """        
         df = pd.read_csv("data/vaccination_tweets.csv")
         top_tags=df[col_name].value_counts().sort_values(ascending=False)
 
-        return (list(top_tags[:k].index) , list(top_tags[:k].values))
+        return list(top_tags[:k].index) , list(top_tags[:k].values)
 
     def sentiment_pie(self):
+        """[Get sentimental analysis ]
+
+        Returns:
+            [string]: [sentiment "positive" "neutral" "negative"]
+            [integers]: [values]
+        """        
         df = pd.read_csv("data/vaccination_tweets.csv")
         neutral_thresh = 0.05
 
@@ -237,58 +364,73 @@ class Pie:
         labels = ['positive', 'neutral', 'negative']
         sizes = [positive, neutral, negative]
 
-        return (labels, sizes)
+        return labels, sizes
 
-# class Wordcloud:
-#     def __init__(self):
-#         df = pd.read_csv("data/vaccination_tweets.csv")
-
-#     def show_wordcloud(self, text, title):
-#         stop_words = set(STOPWORDS)
-#         stop_words.update(["t", "co", "https", "amp", "U"])
-#         wordcloud = WordCloud(stopwords=stop_words, scale=4, max_font_size=50, 
-#                 max_words=500,background_color="black").generate(text)
-
-#         fig = plt.figure(1, figsize=(8,8))
-#         plt.axis('off')
-#         fig.suptitle(title)
-#         # plt.imshow(wordcloud, interpolation='bilinear')
-#         # plt.savefig(os.path.join(self.word_dir, title + ".png"))
-
-#     def text_wordcloud(self, df, col_name):
-#         data = df[col_name]
-#         title = 'Prevalent' + col_name + ' in tweets'     
-#         text = " ".join(t for t in data.dropna())
-
-#         self.show_wordcloud(text, title)
-
-
-#     def country_map(self, df):
-#         loc_df = df['user_location'].str.split(',',expand=True)
-#         loc_df=loc_df.rename(columns={0:'fst_loc',1:'snd_loc'})
-#         # Remove Spaces 
-#         loc_df['snd_loc'] = loc_df['snd_loc'].str.strip()
-
-#         # Rename States 
-#         state_fix = {'Ontario': 'Canada','United Arab Emirates': 'UAE','TX': 'USA','NY': 'USA'
-#                         ,'FL': 'USA','England': 'UK','Watford': 'UK','GA': 'USA','IL': 'USA'
-#                         ,'Alberta': 'Canada','WA': 'USA','NC': 'USA','British Columbia': 'Canada','MA': 'USA','ON':'Canada'
-#                     ,'OH':'USA','MO':'USA','AZ':'USA','NJ':'USA','CA':'USA','DC':'USA','AB':'USA','PA':'USA','SC':'USA'
-#                     ,'VA':'USA','TN':'USA','New York':'USA','Dubai':'UAE','CO':'USA'}
-
-#         loc_df = loc_df.replace({"snd_loc": state_fix}) 
-#         df_loc = pd.DataFrame(loc_df['snd_loc'].value_counts()[:20]).reset_index()
-
-#         fig = px.choropleth(df_loc, locations = df_loc['index'],
-#                             color = df_loc['snd_loc'],locationmode='country names', hover_name = df_loc['snd_loc'], 
-#                             color_continuous_scale = px.colors.sequential.Inferno)
-#         fig.write_image(os.path.join(self.word_dir, "map.png"))  
-
-class Line:
+class Wordcloud:
     def __init__(self):
         df = pd.read_csv("data/vaccination_tweets.csv")
 
+    def show_wordcloud(self, text, title):
+        stop_words = set(STOPWORDS)
+        stop_words.update(["t", "co", "https", "amp", "U"])
+        wordcloud = WordCloud(stopwords=stop_words, scale=4, max_font_size=50, 
+                max_words=500,background_color="black").generate(text)
+
+        fig = plt.figure(1, figsize=(8,8))
+        plt.axis('off')
+        fig.suptitle(title)
+        # plt.imshow(wordcloud, interpolation='bilinear')
+        # plt.savefig(os.path.join(self.word_dir, title + ".png"))
+
+    def text_wordcloud(self, col_name):
+        df = pd.read_csv("data/vaccination_tweets.csv")
+        data = df[col_name]
+        title = 'Prevalent' + col_name + ' in tweets'     
+        text = " ".join(t for t in data.dropna())
+        stop_words = set(STOPWORDS)
+        stop_words.update(["t", "co", "https", "amp", "U"])
+
+        wordcloud = WordCloud(stopwords=stop_words, scale=4, max_font_size=50, 
+                max_words=500,background_color="black").generate(text)
+
+        fig = plt.figure(1, figsize=(8,8))
+        plt.axis('off')
+        plt.imshow(wordcloud, interpolation='bilinear')
+        # plt.savefig(os.path.join(self.word_dir, title + ".png"))
+        
+        return wordcloud
+
+
+    def country_map(self, df):
+        loc_df = df['user_location'].str.split(',',expand=True)
+        loc_df=loc_df.rename(columns={0:'fst_loc',1:'snd_loc'})
+        # Remove Spaces 
+        loc_df['snd_loc'] = loc_df['snd_loc'].str.strip()
+
+        # Rename States 
+        state_fix = {'Ontario': 'Canada','United Arab Emirates': 'UAE','TX': 'USA','NY': 'USA'
+                        ,'FL': 'USA','England': 'UK','Watford': 'UK','GA': 'USA','IL': 'USA'
+                        ,'Alberta': 'Canada','WA': 'USA','NC': 'USA','British Columbia': 'Canada','MA': 'USA','ON':'Canada'
+                    ,'OH':'USA','MO':'USA','AZ':'USA','NJ':'USA','CA':'USA','DC':'USA','AB':'USA','PA':'USA','SC':'USA'
+                    ,'VA':'USA','TN':'USA','New York':'USA','Dubai':'UAE','CO':'USA'}
+
+        loc_df = loc_df.replace({"snd_loc": state_fix}) 
+        df_loc = pd.DataFrame(loc_df['snd_loc'].value_counts()[:20]).reset_index()
+
+        fig = px.choropleth(df_loc, locations = df_loc['index'],
+                            color = df_loc['snd_loc'],locationmode='country names', hover_name = df_loc['snd_loc'], 
+                            color_continuous_scale = px.colors.sequential.Inferno)
+        fig.write_image(os.path.join(self.word_dir, "map.png"))  
+
+class Line:
+
     def month_engagement(self):
+        """[Get monthly engagement]
+
+        Returns:
+            [strings]: [months]
+            [integers]: [values]            
+        """        
         df = pd.read_csv("data/vaccination_tweets.csv")
         df['date']=pd.to_datetime(df['date'])
         df['total_engagement']=df['retweets']+df['favorites']
@@ -297,9 +439,18 @@ class Line:
         df['date'] = df['date'].dt.strftime('%m/%y')       
         day_month = df.groupby('date', sort=False, as_index=False).agg({'total_engagement':'sum'})
 
-        return (list(day_month['date'].values), list(day_month['total_engagement'].values))     
+        return list(day_month['date'].values), list(day_month['total_engagement'].values)
 
     def sentiment_date(self, sentiment_type):
+        """[Get daily sentimental analysis positive, neutral, negative]
+
+        Args:
+            sentiment_type ([integer]): [sentiment "positive" "neutral" "negative"]
+
+        Returns:
+            [strings]: [dates]
+            [integers]: [values]     
+        """        
         analyser = SentimentIntensityAnalyzer()
 
         df=pd.read_csv('data/vaccination_tweets.csv')
@@ -332,6 +483,6 @@ class Line:
         elif sentiment_type == "neutral":
             data = timeline[timeline['sentiment'].str.contains("Neutral")]   
 
-        return (list(data['date'].values), list(data['vals'].values))
+        return list(data['date'].values), list(data['vals'].values)
 
 
